49eb33ee109c63d348adc765ab3e9f7192e8c309
ritikv03 <103930935+ritikv03@users.noreply.github.com>
2023-11-01 22:45:44 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+
+function openNav() {
+    document.getElementById("myNav").classList.toggle("menu_width");
+    document.querySelector(".custom_menu-btn").classList.toggle("menu_btn-style");
+}
+
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(40.712775, -74.005973),
+        zoom: 18,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+
+$(document).on("click", '[data-toggle="lightbox"]', function (event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+});
